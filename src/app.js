48f36b7473cb275f23ec3a79462b707b5def6d33
Will Williams <wwilliams@indeed.com>
2024-01-08 09:44:14 -0800

    Req 2: Enable CORS, write route and server error handling (404 Not Found, 500 Server Error)

--- src/app.js
+++ src/app.js
@@ -2,10 +2,26 @@ if (process.env.USER) require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
 const moviesRouter = require("./movies/movies.router");
 const theatersRouter = require("./theaters/theaters.router");
 const reviewsRouter = require("./reviews/reviews.router");
 
 // TODO: Add your code here
+app.use(cors());
+app.use(express.json());
+
+// Not found handler
+app.use((req, res, next) => {
+  next({ status: 404, message: `Not found: ${req.originalUrl}` });
+});
+
+// Error handler
+app.use((error, req, res, next) => {
+  console.error(error);
+  const { status = 500, message = "Something went wrong!" } = error;
+  res.status(status).json({ error: message });
+});
 
 module.exports = app;
