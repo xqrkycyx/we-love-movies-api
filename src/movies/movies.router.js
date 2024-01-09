2b45626cde71f7e34431ee365f59d382cdbb093d
Will Williams <wwilliams@indeed.com>
2024-01-08 18:29:24 -0800

    Req 5d: Returns all theaters for the specified movie

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -13,7 +13,12 @@ const theatersRouter = require("../theaters/theaters.router");
 // Refer back to Module 4-6 (Advanced Tips):
 // Example was nesting the '/pastes' router within the '/users' router, i.e., '/users/:userId/pastes')
 // Lesson instruction: "Add the following BEFORE ANY OTHER routes:"
-router.use("/:movieId([0-9]+)/reviews", controller.movieExists, reviewsRouter); // controller.userExists in front of pastesRouter to enable 404 response if user Id doesn't exist on users/x/pastes
+router.use("/:movieId([0-9]+)/reviews", controller.movieExists, reviewsRouter); // controller.movieExists in front of reviews router to enable 404 response if movieId doesn't exist
+router.use(
+  "/:movieId([0-9]+)/theaters",
+  controller.movieExists,
+  theatersRouter
+);
 // --------------------------------------------------------
 
 router.route("/:movieId([0-9]+)").get(controller.read).all(methodNotAllowed);
